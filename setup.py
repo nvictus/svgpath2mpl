c8d3d9ef2bc97c8ba6a3e4e01e92faca6404f2eb
Nezar Abdennur <nabdennur@gmail.com>
2022-07-17 15:53:32 -0400

    Add matplotlib plugin classifier

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ classifiers = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
+    "Framework :: Matplotlib",
 ]
 
 
